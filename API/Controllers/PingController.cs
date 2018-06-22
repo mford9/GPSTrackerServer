5341e2f36e1a95db73a9b85554bc5d1046355c07
Unknown <mattford09@gmail.com>
2018-06-22 16:58:15 +0100

    Add API project and ping service

---
+++ API/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ford.Tracker.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Ping")]
+    public class PingController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
